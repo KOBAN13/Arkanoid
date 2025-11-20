6d9be7c2c6c75fc4412f94b62e243e1f443f3ade
KOBAN13 <135891978+KOBAN13@users.noreply.github.com>
2025-11-20 23:09:34 +0400

    Add block spawning through pool

--- Assets/Scripts/Field/GameFieldService.cs
+++ Assets/Scripts/Field/GameFieldService.cs
@@ -1,4 +1,7 @@
-﻿using Field.Matrix;
+using System.Collections.Generic;
+using Field.Matrix;
+using Model;
+using Pool;
 using UnityEngine;
 
 namespace Field
@@ -6,15 +9,42 @@ namespace Field
     public class GameFieldService : IGameFieldService
     {
         private Matrix<EFieldCellType> _field;
-        
+        private readonly IGenericObjectPool<BlockView> _blockPool;
+        private readonly Dictionary<Vector2Int, BlockView> _blocks = new();
+        private Transform _blocksParent;
+
+        public GameFieldService(IGenericObjectPool<BlockView> blockPool)
+        {
+            _blockPool = blockPool;
+        }
+
+        public void Initialize(Matrix<EFieldCellType> field, BlockView blockPrefab, Transform blocksParent)
+        {
+            _field = field;
+            _blocksParent = blocksParent;
+
+            _blockPool.Initialize(blockPrefab);
+        }
+
         public void CreateBlock(Vector2Int position)
         {
             InitializeCell(position, EFieldCellType.Block);
+
+            var blockView = _blockPool.GetObject();
+            blockView.transform.SetParent(_blocksParent, false);
+            blockView.transform.localPosition = new Vector3(position.x, position.y, 0f);
+
+            _blocks[position] = blockView;
         }
 
         public void DeleteBlock(Vector2Int position)
         {
             DestoryCell(position);
+
+            if (_blocks.Remove(position, out var blockView))
+            {
+                _blockPool.ReturnObject(blockView);
+            }
         }
 
         public EFieldCellType GetCellType(Vector2Int position)
--- Assets/Scripts/Field/IGameFieldService.cs
+++ Assets/Scripts/Field/IGameFieldService.cs
@@ -1,10 +1,12 @@
-﻿using Field.Matrix;
+using Field.Matrix;
+using Model;
 using UnityEngine;
 
 namespace Field
 {
     public interface IGameFieldService
     {
+        void Initialize(Matrix<EFieldCellType> field, BlockView blockPrefab, Transform blocksParent);
         void CreateBlock(Vector2Int position);
         void DeleteBlock(Vector2Int position);
         EFieldCellType GetCellType(Vector2Int position);
